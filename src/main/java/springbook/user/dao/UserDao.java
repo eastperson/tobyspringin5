7d036fa7df9b12219e7d09e42db7e71d2d10d8de
EP <kjuioqq8@gmail.com>
2022-08-31 22:02:13 +0900

    feat: 1.1.2 UserDao

---
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,57 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    // User 등록
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost/springbook", "spring", "book");
+
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "insert into users(id, name, password) values (?,?,?)");
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    // User 조회
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost/springbook", "spring", "book");
+
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "select * from users where id = ?");
+        preparedStatement.setString(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getString("id"));
+        user.setId(resultSet.getString("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+}
