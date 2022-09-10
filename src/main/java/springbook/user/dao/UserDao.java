34afd0e6d62535dc3062a5dc9ff5ae2be289aa43
EP <kjuioqq8@gmail.com>
2022-09-10 20:15:08 +0900

    3.2.2 분리와 재사용을 위한 디자인 패턴 적용

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -2,6 +2,8 @@ package springbook.user.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import springbook.user.dao.strategy.DeleteAllStatement;
+import springbook.user.dao.strategy.StatementStrategy;
 import springbook.user.domain.User;
 
 import java.sql.Connection;
@@ -17,6 +19,21 @@ public class UserDao {
     @Autowired
     public void setConnectionMaker(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy statementStrategy) throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = connectionMaker.makeNewConnection();
+            preparedStatement = statementStrategy.makePreparedStatement(connection);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (preparedStatement != null) { try { preparedStatement.close(); } catch (SQLException e) {} }
+            if (connection != null) { try { connection.close(); } catch (SQLException e) {} }
+        }
     }
 
     // User 등록
@@ -92,34 +109,10 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException, ClassNotFoundException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        try {
-            // 예외가 발생할 가능성이 있는 코드를 모두 try 블록으로 묶어준다.
-            connection = connectionMaker.makeNewConnection();
-            preparedStatement = connection.prepareStatement("delete from users");
-            preparedStatement.executeUpdate();
-        // 예외가 발생했을 때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다. 아직은 예외를 다시 메소드 밖으로 던지는 것밖에 없다.
-        } catch (Exception e) {
-            throw e;
-        // finally 이므로 try 블록에서 예외가 발생했을 때나 안 했을때나 모두 실행된다.
-        } finally {
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                // preparedStatement.close()에서 SQLException이 발생할 수 있기 때문에 이를 잡아줘야 한다.
-                } catch (SQLException e) {
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-
-                }
-            }
-        }
+        // 선정한 전략 클래스의 오브젝트 생성
+        StatementStrategy statementStrategy = new DeleteAllStatement();
+        // 컨텍스트 호출. 전략 오브젝트 전달달
+       jdbcContextWithStatementStrategy(statementStrategy);
     }
 
     public int getCount() throws SQLException, ClassNotFoundException {
